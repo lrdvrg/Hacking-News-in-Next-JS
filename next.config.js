9b9554fffe1c828dbb8c5e9785360467cf9d29f7
Isaí Vargas <isai.vargas@applydigital.com>
2023-11-16 09:48:52 -0400

    feat: init project in git

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_URL: process.env.API_URL,
+        CATEGORY_LOCALSTORAGE_KEY: process.env.CATEGORY_LOCALSTORAGE_KEY,
+        FAVORITES_LOCALSTORAGE_KEY: process.env.FAVORITES_LOCALSTORAGE_KEY,
+      },
+}
 
 module.exports = nextConfig
